71c55ecc69ad8608e85e20b7e454094c3f6d9410
Swapnil Kasliwal <skasliwal12@gmail.com>
2018-02-19 13:41:29 -0500

    new scratch file

--- node-dota2/scratch.js
+++ node-dota2/scratch.js
@@ -2,20 +2,22 @@ var fs = require('fs');
 const ProtoBuf = require('protobufjs');
 var path = require('path');
 
-var body = Buffer.from([]);
-fs.readFileSync('./test.txt', function read(err, data) {
-  if (err) {
-    throw err;
-  }
-  body = data;
-  console.log(body);
-});
+var buffer = fs.readFileSync((path.join(__dirname) + '/test.txt'));
+console.log('buffer length is ' + buffer.byteLength);
 
 var builder = ProtoBuf.loadSync(path.join(__dirname) + '/proto/dota_match_metadata.proto');
-var decoded_message = builder.CDOTAMatchMetadataFile;
-console.log(decoded_message);
+// var decoded_message = builder.CDOTAMatchMetadataFile.decode(buffer);
+// console.log(decoded_message);
 
-
+try {
+  var decodedMessage = builder.CDOTAMatchMetadataFile.decode(buffer);
+} catch (e) {
+  if (e instanceof ProtoBuf.util.ProtocolError) {
+    console.log(e.instance);
+  } else {
+    console.log('wire format is invalid');
+  }
+};
 
 
 
